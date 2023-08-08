73f12808758d579585ec796948b3757b8f2095ac
Iamneo <techadmin@iamneo.ai>
2023-08-08 14:24:55 +0000

    Backup Commit

---
+++ jspservlet-app-stringmania/test/java/testing/TestStringAdvanceMethod.java
@@ -0,0 +1,31 @@
+package testing;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import utility.StringAdvanceMethod;
+
+public class TestStringAdvanceMethod {
+	StringAdvanceMethod sam = new StringAdvanceMethod();
+
+	@Test
+	public void testSetInputsMethod() {
+		
+	}
+
+	@Test
+	public void testConcatMethod() {
+		
+	}
+
+	@Test
+	public void testSplitMethod() {
+		
+	}
+
+	@Test
+	public void testIndexOfMethod() {
+		
+	}
+}
